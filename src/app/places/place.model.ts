77fdfe7e70669ad3ef46204099890b586c1f0555
nigeldkat <nigeldkat@hotmail.com>
2019-05-17 15:21:14 -0400

    template forms

--- src/app/places/place.model.ts
+++ src/app/places/place.model.ts
@@ -1,9 +1,13 @@
+import { DeprecatedCurrencyPipe } from '@angular/common';
+
 export class Place {
   constructor(
     public id: string,
     public title: string,
     public decription: string,
     public imageUrl: string,
-    public price: number
+    public price: number,
+    public availableFrom: Date,
+    public availaleTo: Date
   ) {}
 }
